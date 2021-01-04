96db7f361d9214c3dff722d692ebe7e4ca19e415
Ojaswi Awasthi <65769340+Ojaswi2000@users.noreply.github.com>
2021-01-05 00:52:55 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import {React,Component} from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+export default class App extends Component
+{
+  render()
+  {
+    return(
+      <View>
+        <Text>My Chat App</Text>
+      </View>
+    );
+  }
+}
